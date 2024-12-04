a63e2faaf54e19a0b17aae34d9b9af71cb2d99b8
griffin-kimani <baxgriffin@gmail.com>
2024-12-04 21:16:09 +0300

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './about/About';
-export { default as Services } from './services/Services';
-export { default as Contactus } from './contactus/Contactus';
-export { default as Navbar } from './navbar/Navbar';
-export { default as Talktous } from './talktous/Talktous';
-export { default as Chatbot } from './chatbot/Chatbot';
